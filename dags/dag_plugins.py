ff4f55d8708be2cea75e77e1b895d18c15e9cea8
lawrencejews <lawrencejews1@gmail.com>
2025-04-29 10:17:26 +0900

    updated the localExecutor file

---
+++ dags/dag_plugins.py
@@ -0,0 +1,38 @@
+from airflow import DAG 
+from datetime import datetime, timedelta
+from airflow.operators import DataTransferOperator,FileCountSensor
+from airflow.hooks import MySQLToPostgresHook
+from airflow.operators.python_operator import PythonOperator
+
+with DAG(
+  'plugin_dag',
+  schedule_interval=timedelta(1),
+  start_date=datetime(2025, 4, 27),
+  catchup=False
+  ) as dag:
+  
+  t1 = DataTransferOperator(
+    task_id = 'data_transfer',
+    source_file_path ='/usr/local/airflow/plugins/source.txt',
+    dest_file_path = '/usr/local/airflow/plugins/destination.txt', 
+    delete_list = ['Airflow', 'is'],
+  )
+  
+  def trigger_hook():
+    MySQLToPostgresHook().copy_table('mysql_conn', 'postgres_conn')
+    print("done")
+  
+  t2 = FileCountSensor(
+    task_id = 'file_count_sensor',
+    dir_path ='/usr/local/airflow/plugins',
+    conn_id = 'fs_default', 
+    poke_interval = 5,
+    timeout = 100,
+  )
+  
+  t3 = PythonOperator(
+    task_id = 'mysql_to_postgres',
+    python_callable = trigger_hook,
+  )
+  
+  t1
